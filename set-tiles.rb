6fbb7ab88099f6634e812aa77ada0d83246919bf
Hendrik Albers <mail@hendrik.dev>
2020-09-22 20:50:13 +0200

    output model and image for tiles

--- set-tiles.rb
+++ set-tiles.rb
@@ -2,12 +2,30 @@
 puts ARGV.first
 tileset = File.read(ARGV.first)
 tiles = []
+id = -1
+model = ''
+image = ''
 for line in tileset.lines do
   m = line.match(/\[TILE(\d+)\]/)
   if m
-    tiles << m[1].to_i
+    tiles << {id: id, model: model, image: image} if id != -1
+    id = m[1]
+    next
+  end
+  m = line.match(/Model=(\w*)/)
+  if m
+    model = m[1]
+    next
+  end
+  m = line.match(/ImageMap2D=(\w*)/)
+  if m
+    image = m[1]
+    next
   end
 end
+tiles << {id: id, model: model, image: image}
 
 puts 'Number of tiles: ' + tiles.count.to_s
-p tiles
+for tile in tiles do
+  puts "#{tile[:id]}: #{tile[:model]}, #{tile[:image]}"
+end
