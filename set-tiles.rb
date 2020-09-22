e4ed8549945777c8e79dd5f0b7b77aed50d074d2
Hendrik Albers <mail@hendrik.dev>
2020-09-22 22:20:44 +0200

    add strref and name if palstd.itp.json and dialog.csv exist

--- set-tiles.rb
+++ set-tiles.rb
@@ -1,6 +1,32 @@
 #!/usr/bin/env ruby
+require 'csv'
+require 'json'
+
 puts ARGV.first
 tileset = File.read(ARGV.first)
+palstd = File.basename(ARGV.first).sub(/\.set$/, 'palstd.itp.json')
+
+def flatten_list(list)
+  # returns a flat array of [resref, strref] pairs
+  result = []
+  for el in list do
+    if el['LIST']
+      result.concat(flatten_list(el['LIST']['value']))
+    else
+      result << [el['RESREF']['value'], el['STRREF']['value']]
+    end
+  end
+  return result
+end
+
+if File.exists?(palstd)
+  strrefs = flatten_list(JSON.parse(File.read(palstd))['MAIN']['value']).to_h
+end
+
+if File.exists?('dialog.csv')
+  tlk = CSV.read('dialog.csv').map{|row| [row.first.to_i, row.last]}.to_h
+end
+
 tiles = []
 id = -1
 model = ''
@@ -27,5 +53,13 @@ tiles << {id: id, model: model, image: image}
 
 puts 'Number of tiles: ' + tiles.count.to_s
 for tile in tiles do
-  puts "#{tile[:id]}: #{tile[:model]}, #{tile[:image]}"
+  output = [tile[:id].to_s, tile[:model], tile[:image]]
+  if strrefs
+    strref = strrefs[tile[:model]]
+    if strref
+      output << strref
+      output << tlk[strref] if tlk && tlk[strref]
+    end
+  end
+  puts output.join(', ')
 end
