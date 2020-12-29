eabc263ecc11053253eb95b04bacadf8dc915478
Hendrik Albers <mail@hendrik.dev>
2020-12-29 13:55:33 +0100

    updates to winter tile conversion script

--- winter.rb
+++ winter.rb
@@ -2,27 +2,41 @@ require 'fileutils'
 
 models = File.read('models.txt').split("\n")
 
-set = File.read('hak/sfwintertileset/sfwinter.set')
 models.each do |mdl|
- set.sub!("Model=#{mdl}", "Model=#{mdl}_w")
-end
-File.write('hak/sfwintertileset/sfwinter.set', set)
-
-models.each do |mdl|
- FileUtils.cp "hak/sfarkontileset1/#{mdl}.mdl", "hak/sfwintertileset/#{mdl}_w.mdl"
- FileUtils.cp "hak/sfarkontileset1/#{mdl}.wok", "hak/sfwintertileset/#{mdl}_w.wok"
+  if mdl.length + 2 > 16
+    puts "#{mdl}: name too long with added _w"
+  end
 end
 
+set = File.read('hak/sfwinterrural/sfwinterrural.set')
 models.each do |mdl|
-  f = File.read("hak/sfwintertileset/#{mdl}_w.mdl")
+  set.sub!("Model=#{mdl}", "Model=#{mdl}_w")
+  set.sub!("WalkMesh=#{mdl}", "WalkMesh=#{mdl}_w")
+end
+File.write('hak/sfwinterrural/sfwinterrural.set', set)
+
+models.each do |mdl|
+  FileUtils.cp "hak/sfarkontileset1/#{mdl}.mdl", "hak/sfwinterrural/#{mdl}_w.mdl"
+  FileUtils.cp "hak/sfarkontileset1/#{mdl}.wok", "hak/sfwinterrural/#{mdl}_w.wok"
+
+  #FileUtils.cp "hak/sfarkontileset1/#{mdl}.mdl", "/home/hal/nwntools/cm/input/#{mdl}.mdl"
+
+  #FileUtils.cp "/home/hal/nwntools/cm/output/#{mdl}.mdl", "hak/sfwinterrural/#{mdl}_w.mdl"
+  #FileUtils.cp "/home/hal/nwntools/cm/output/#{mdl}.wok", "hak/sfwinterrural/#{mdl}_w.wok"
+end
+
+models.each do |mdl|
+  f = File.read("hak/sfwinterrural/#{mdl}_w.mdl")
   f.gsub!(mdl, "#{mdl}_w")
   f.gsub!(/bitmap ttr01_grass02/i, 'bitmap tts02_grass02')
-  File.write("hak/sfwintertileset/#{mdl}_w.mdl", f)
+  f.gsub!(/bitmap ttr01_road01/i, 'bitmap tts02_road01')
+  File.write("hak/sfwinterrural/#{mdl}_w.mdl", f)
 end
 
 models.each do |mdl|
-  lines = File.read("hak/sfwintertileset/#{mdl}_w.mdl").split("\r\n")
+  lines = File.read("hak/sfwinterrural/#{mdl}_w.mdl").split(/\r?\n/)
   aabb_idx = lines.find_index{|l| l.strip.start_with?('node aabb')}
+  p mdl if aabb_idx.nil? # binary mdl
   faces_idx = -1
   aabb_end_idx = -1
   lines.each_with_index do |l, idx|
@@ -37,5 +51,5 @@ models.each do |mdl|
   for idx in faces_idx ... aabb_end_idx do
     lines[idx].sub!(/ 3$/, ' 19')
   end
-  File.write("hak/sfwintertileset/#{mdl}_w.mdl", lines.join("\r\n"))
+  File.write("hak/sfwinterrural/#{mdl}_w.mdl", lines.join("\r\n"))
 end
